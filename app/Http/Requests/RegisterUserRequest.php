66cb769e7b7e6088e91c1ee1453ef0ab3566a6bf
NhuND <nhu.nguyen@luxstay.com>
2019-08-27 18:03:57 +0700

    api slide and sch

--- app/Http/Requests/RegisterUserRequest.php
+++ app/Http/Requests/RegisterUserRequest.php
@@ -16,6 +16,7 @@ class RegisterUserRequest extends AuthorizedFormRequest
             'email'    => 'required|email|unique:users',
             'password' => 'required|confirmed',
             'phone'    => 'phone|unique:users',
+            'full_name'=> 'unique:users',
         ];
     }
 
@@ -28,6 +29,7 @@ class RegisterUserRequest extends AuthorizedFormRequest
             'password.required'  => ' Mật khẩu không được để trống.',
             'password.min'       => ' Mật khẩu tối thiểu 6 ký tự.',
             'password.confirmed' => 'Mật khẩu xác nhận không đúng',
+            'full_name.unique'   => 'Tên đã được sử dụng',
         ];
     }
 }
