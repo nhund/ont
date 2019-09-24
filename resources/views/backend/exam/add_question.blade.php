3acc239d558b65db20b47e2797bc8856bec2e83a
NhuND <nhu.nguyen@luxstay.com>
2019-09-24 17:56:21 +0700

    api report and searching courses of

---
+++ resources/views/backend/exam/add_question.blade.php
@@ -0,0 +1,30 @@
+<div class="row top10">
+    <div class="col-xs-12">
+        <div class="panel panel-grape panel-bod">
+            <div class="panel-heading"><h2>Thông tin chung</h2></div>
+            <div class="panel-body">
+                <div class="table-responsive SourceSansProSemibold">
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <td></td>class="text-bold">Tìm câu hỏi</td>
+                                <td><input class="form-control"></td>
+                                <td><button type="submit">tìm</button> </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th class="text-bold">ID</th>
+                            <th>Tên</th>
+                            <th>Loại</th>
+                            <th>Hành động</th>
+                        </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
