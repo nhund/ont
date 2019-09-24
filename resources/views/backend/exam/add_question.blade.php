96bb25c1bdf404b2af7339a6c5a34fbabde3049b
NhuND <nhu.nguyen@luxstay.com>
2019-09-24 18:03:51 +0700

    api report and searching courses of

--- resources/views/backend/exam/add_question.blade.php
+++ resources/views/backend/exam/add_question.blade.php
@@ -1,18 +1,32 @@
 <div class="row top10">
     <div class="col-xs-12">
         <div class="panel panel-grape panel-bod">
-            <div class="panel-heading"><h2>Thông tin chung</h2></div>
+            <div class="panel-heading"><h2>Chọn cậu hỏi vào đề thi</h2></div>
             <div class="panel-body">
                 <div class="table-responsive SourceSansProSemibold">
-                    <table class="table">
-                        <tbody>
-                            <tr>
-                                <td class="text-bold">Tìm câu hỏi</td>
-                                <td><input class="form-control"></td>
-                                <td><button type="submit">tìm</button> </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <form method="get" action="">
+                        <table class="table">
+                            <tbody>
+                                <tr>
+                                    <td class="text-bold"><label for="keySearch">Tìm câu hỏi</label></td>
+                                    <td><input id="keySearch" class="form-control" type="text" name="keySearch"> <button class="btn btn-primary" type="submit">tìm kiếm</button> </td>
+                                </tr>
+                                <tr>
+                                    <td class="text-bold"><label for="keySearch">Câu hỏi thuộc phần</label></td>
+                                    <td><select name="part">
+                                            <option value="1">Phần 1</option>
+                                            <option value="2">Phần 2</option>
+                                            <option value="3">Phần 3</option>
+                                            <option value="4">Phần 4</option>
+                                            <option value="5">Phần 5</option>
+                                            <option value="6">Phần 6</option>
+                                        </select>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </form>
+
                     <table class="table">
                         <thead>
                         <tr>
