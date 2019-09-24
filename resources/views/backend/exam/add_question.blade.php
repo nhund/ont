958b0df234817b239dc563bbbe3f18dadd3a78b1
NhuND <nhund@gmail.com>
2019-09-24 23:38:14 +0700

    register user

--- resources/views/backend/exam/add_question.blade.php
+++ resources/views/backend/exam/add_question.blade.php
@@ -50,7 +50,7 @@
                                         <td>{{$suggestQuestion->id}}</td>
                                         <td>{!! $suggestQuestion->question !!}</td>
                                         <td>{!! $suggestQuestion->type() !!}</td>
-                                        <td><input type="checkbox" name="question_id[]" value="{{$suggestQuestion->id}}"></td>
+                                        <td><input type="checkbox" name="question_id" value="{{$suggestQuestion->id}}"/></td>
                                     </tr>
                                 @endforeach
                             </tbody>
