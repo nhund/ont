2d5ad4db7949a8564b5f5e6712305a3ae88ea0f5
NhuND <nhund@gmail.com>
2019-09-30 00:25:59 +0700

    register user

--- resources/views/include/backend/detail_modal.blade.php
+++ resources/views/include/backend/detail_modal.blade.php
@@ -159,3 +159,59 @@
         </div>
     </div>
 </div>
+
+{{-- update score exam--}}
+<div class="modal fade" id="update-part-exam" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header text-center">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h1 class="modal-title">Cập nhật điểm của phần kiểm tra</h1>
+            </div>
+            <form id="form-update-part-exam" method="POST" action="{{ route('update.partExam') }}">
+                <div class="modal-body">
+                    <input type="hidden" name="lesson_id" id="les_id" value="">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <div class="row">
+                        <div class="col-sm-8">Tên bài tập</div>
+                        <div class="col-sm-4">Trạng thái</div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-2">phần 1</div>
+                        <div class="col-sm-3"><input type="number" name="part_1"></div>
+                        <div class="col-sm-2">phần 2</div>
+                        <div class="col-sm-3"><input type="number" name="part_2"></div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-2">phần 3</div>
+                        <div class="col-sm-3"><input type="number" name="part_3"></div>
+                        <div class="col-sm-2">phần 4</div>
+                        <div class="col-sm-3"><input type="number" name="part_4"></div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-2">phần 5</div>
+                        <div class="col-sm-3"><input type="number" name="part_5"></div>
+                        <div class="col-sm-2">phần 6</div>
+                        <div class="col-sm-3"><input type="number" name="part_6"></div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-2">phần 7</div>
+                        <div class="col-sm-3"><input type="number" name="part_7"></div>
+                        <div class="col-sm-2">phần 8</div>
+                        <div class="col-sm-3"><input type="number" name="part_8"></div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-2">phần 9</div>
+                        <div class="col-sm-3"><input type="number" name="part_9"></div>
+                        <div class="col-sm-2">phần 10</div>
+                        <div class="col-sm-3"><input type="number" name="part_10"></div>
+                    </div>
+                </div>
+            </form>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary">Lưu</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Hủy</button>
+            </div>
+        </div>
+    </div>
+</div>
