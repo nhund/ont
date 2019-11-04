eb6add17a997c55467ce22e53bf2b61aecd5def6
Thanh <thanhdv193@gmail.com>
2019-11-04 17:23:06 +0700

    app : update code

--- resources/views/app/dien_tu_doan_van.blade.php
+++ resources/views/app/dien_tu_doan_van.blade.php
@@ -1,6 +1,6 @@
 <div class="question-app-dien-tu">
     <div class="content">
-
+        {!! $item->question_display !!}
     </div>
     <div class="explain">
         <div class="title">Giải thích</div>
