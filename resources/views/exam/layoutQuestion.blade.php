7b59dec96ea317e55cd127b3a9fd044158eed8af
NhuND <nhu.nguyen@luxstay.com>
2020-02-24 17:44:25 +0700

    total sc

--- resources/views/exam/layoutQuestion.blade.php
+++ resources/views/exam/layoutQuestion.blade.php
@@ -107,7 +107,7 @@
                                         </div>
                                         <div class="col-md-6 title-score">
                                             <h2><span class="score-text"><strong>Điểm</strong></span></h2>
-                                            <span><strong class="minute-1">{{number_format($var['userExam']->doing_time, 1, ',','')}}  Phút</strong></span>
+                                            <span><strong class="minute-1">{{ $var['userExam']->doing_time}}  Phút</strong></span>
                                         </div>
                                     </div>
                                 </div>
@@ -125,7 +125,7 @@
                                                     <span>{{$rank->user->name_full ?? 'Ẩn danh'}}</span>
                                                 </td>
                                                 <td>{{$rank->highest_score}} Điểm</td>
-                                                <td>{{number_format($rank->doing_time,1, ',','')}} Phút</td>
+                                                <td>{{$rank->doing_time}} Phút</td>
                                             </tr>
                                         @endforeach
                                         </tbody>
