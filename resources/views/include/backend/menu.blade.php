6a85cc557e0a91777cfa7e92b01556498555629d
NhuND <nhu.nguyen@luxstay.com>
2020-03-02 09:54:59 +0700

    total sc

--- resources/views/include/backend/menu.blade.php
+++ resources/views/include/backend/menu.blade.php
@@ -46,33 +46,24 @@
                             <li><a class="{{ url()->current() == route('admin.school.index') ? 'active':''  }}" href="{{ route('admin.school.index') }}">Danh sách trường học</a></li>
                         </ul>
                     </li>
-                    {{-- <li class="hasChild {{ Request()->route()->getPrefix() == 'admin/slider' ? 'active':''  }}"><a href="javascript:;"><i class="fa fa-image"></i><span>Slider</span></a>
-                        <ul class="acc-menu" @if(Request()->route()->getPrefix() == 'admin/slider' ? 'active':'')  style="display: block" @endif>
-                            <li ><a class="{{ url()->current() == route('admin.slider.add') ? 'active':''  }}" href="{{ route('admin.slider.add') }}">Thêm slider</a></li>
-                            <li><a class="{{ url()->current() == route('admin.slider.index') ? 'active':''  }}" href="{{ route('admin.slider.index') }}">Danh sách slider</a></li>
-                        </ul>
-                    </li> --}}
-                    {{-- <li class="hasChild {{ Request()->route()->getPrefix() == 'admin/founder' ? 'active':''  }}"><a href="javascript:;"><i class="fa fa-anchor"></i><span>Founder</span></a>
-                        <ul class="acc-menu" @if(Request()->route()->getPrefix() == 'admin/founder' ? 'active':'')  style="display: block" @endif>
-                            <li ><a class="{{ url()->current() == route('admin.founder.add') ? 'active':''  }}" href="{{ route('admin.founder.add') }}">Thêm Founder</a></li>
-                            <li><a class="{{ url()->current() == route('admin.founder.index') ? 'active':''  }}" href="{{ route('admin.founder.index') }}">Danh sách Founder</a></li>
-                        </ul>
-                    </li> --}}
-
                     <li class="hasChild"><a href="javascript:;"><i class="fa fa-cog"></i><span>Cài đặt chung</span></a>
                         <ul class="acc-menu">
                             <li ><a class="{{ url()->current() == route('admin.about.index') ? 'active':''  }}" href="{{ route('admin.about.index') }}">Thông tin website</a></li>
-                            <li><a class="{{ url()->current() == route('admin.user_feel.index') ? 'active':''  }}" href="{{ route('admin.user_feel.index') }}">Nhận xét - Đánh giá</a></li>
-                            <li><a class="{{ url()->current() == route('admin.feedback.index') ? 'active':''  }}" href="{{ route('admin.feedback.index') }}">Phản hồi</a></li>
                             <li><a class="{{ url()->current() == route('admin.slider.index') ? 'active':''  }}" href="{{ route('admin.slider.index') }}">Slider</a></li>
                             <li><a class="{{ url()->current() == route('admin.founder.index') ? 'active':''  }}" href="{{ route('admin.founder.index') }}">Founder</a></li>
                             <li><a class="{{ url()->current() == route('admin.menu.index') ? 'active':''  }}" href="{{ route('admin.menu.index') }}">Menu ngang</a></li>
+                            <li><a href="{{ route('admin.toolUpload') }}"><i class="fa fa-image"></i><span>Upload ảnh</span></a></li>
                         </ul>
                     </li>
-                    <li><a href="{{ route('admin.contact.index') }}"><i class="fa fa-comment-o"></i><span>Tin nhắn</span></a></li>                    
+                    <li class="hasChild"><a href="javascript:;"><i class="fa fa-anchor"></i><span>Phản hồi</span></a>
+                       <ul class="acc-menu" @if(Request()->route()->getPrefix() == 'admin/founder' ? 'active':'')  style="display: block" @endif>
+                           <li><a class="{{ url()->current() == route('admin.user_feel.index') ? 'active':''  }}" href="{{ route('admin.user_feel.index') }}">Nhận xét - Đánh giá</a></li>
+                           <li><a class="{{ url()->current() == route('admin.contact.index') ? 'active':''  }}"  href="{{ route('admin.contact.index') }}"><span>Tin nhắn</span></a></li>
+                           <li><a class="{{ url()->current() == route('admin.feedback.index') ? 'active':''  }}" href="{{ route('admin.feedback.index') }}">Phản hồi</a></li>
+                       </ul>
+                   </li>
                     <li><a href="{{ route('code.list') }}"><i class="fa fa-barcode"></i><span>Tạo Mã Code</span></a></li>
-                    <li><a href="{{ route('admin.toolUpload') }}"><i class="fa fa-image"></i><span>Upload ảnh</span></a></li>
-                @else 
+                @else
                    @if(Auth::user()->level == \App\User::USER_TEACHER) 
                         <li><a href="{{ route('addcourse') }}"><i class="fa fa-pencil-square-o"></i><span>Tạo khóa học</span></a></li>     
                    @endif
