2963828fe0dd71426b17baf0f4b69091a7687c4a
NhuND <nhu.nguyen@luxstay.com>
2020-03-13 15:55:37 +0700

    total sc

--- resources/views/exam/ket_qua.blade.php
+++ resources/views/exam/ket_qua.blade.php
@@ -4,7 +4,39 @@
 @endpush
 @section('content')
     <div class="hoclythuyet type_flash_card type_do_new">
-        @include('exam.lam_bai_moi')
+        <div class="header-navigate clearfix mb15" style="color:#F87B54 !important; ">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 pd5 head_course head_flash_Card">
+                        <div class="lesson_name do_new">
+                            <img src="{{ web_asset('public/images/course/icon/icon_bt_moi_48.png') }}">
+                            <div class="name">
+                                <div class="box_head">
+                                    <div title="{{ $var['lesson']->name }}" class="title">{{ $var['lesson']->name }}</div>
+                                    <div class="course_process">Đã làm
+                                        <span class="count_question_done">0</span>/<span class="total_question">{{ count($var['questions']) }}</span>
+                                    </div>
+                                </div>
+                                <div class="progress">
+                                    <div class="progress-bar" role="progressbar" aria-valuenow="70"
+                                         aria-valuemin="0" aria-valuemax="100" style="width:0%">
+                                        <span class="sr-only"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="close_course">
+                            @if($var['lesson']->level == \App\Models\Lesson::LEVEL_2)
+                                <a href="{{ route('course.learn',['title'=>str_slug($var['course']->name),'course_id'=> $var['course']->id, 'lesson_id'=>$var['lesson']->id]) }}" class="fa fa-close"></a>
+                            @else
+                                <a href="{{ route('course.learn',['title'=>str_slug($var['course']->name),'id'=>$var['course']->id]) }}" class="fa fa-close"></a>
+                            @endif
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <section id="hoclythuyet" style="background: #35365e; min-height: 100vh;" class="clearfix flash_card">
             <div class="container">
                 <div class="row">
